798721333f038ff74034d97200b79602bb746f8f
djeang <djeangdev@yahoo.fr>
2015-12-23 00:00:10 +0100

    update

--- org.jerkar.sample-spring-boot-simple/build/def/Build.java
+++ org.jerkar.sample-spring-boot-simple/build/def/Build.java
@@ -1,5 +1,6 @@
 import static org.jerkar.addin.springboot.JkSpringModules.*;
 import org.jerkar.addin.springboot.JkSpringbootBuild;
+import org.jerkar.addin.springboot.JkSpringbootVersionManagement;
 import org.jerkar.api.depmanagement.JkDependencies;
 import org.jerkar.tool.JkImport;
 import org.jerkar.tool.JkInit;
@@ -8,12 +9,18 @@ import org.jerkar.tool.JkInit;
  * @author djeang
  * @formatter:off
  */
-@JkImport({ "org.jerkar:addin-spring-boot:1.2.7.0"})
+@JkImport({ "org.jerkar:addin-spring-boot:1.3.1.0"})
 class Build extends JkSpringbootBuild {
 
     public static void main(String[] args) {
 	JkInit.instanceOf(Build.class, args, "-tests.output").doDefault();
     }
+    
+    @Override
+    protected JkSpringbootVersionManagement versionManagement() {
+        return JkSpringbootVersionManagement.v1_3_0();
+    }
+    
 
     @Override
     protected JkDependencies dependencies() {
