6f58faeeaf548b3208a207a6b6a2bf317a6f08a1
djeang <djeangdev@yahoo.fr>
2019-06-11 21:24:02 +0200

    travis

--- org.jerkar.plugins.spring-boot/jerkar/def/RunPublish.java
+++
@@ -1,9 +0,0 @@
-import org.jerkar.tool.JkInit;
-
-class RunPublish {
-
-    public static void main(String[] args) {
-        JkInit.instanceOf(Build.class, args).javaPlugin.publish();
-    }
-
-}
--- org.jerkar.plugins.spring-boot/jerkar/def/RunRelease.java
+++
@@ -1,9 +0,0 @@
-import org.jerkar.tool.JkInit;
-
-class RunRelease {
-
-    public static void main(String[] args) {
-        JkInit.instanceOf(Build.class, args).release();
-    }
-
-}
