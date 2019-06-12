d21fc3d64f9055a1007593f399089f425f593702
angibaudj <angibaudj>
2019-06-12 17:14:38 +0200

    cleanup

--- dev.jeka.plugins.spring-boot/jeka/def/Build.java
+++ dev.jeka.plugins.spring-boot/jeka/def/Build.java
@@ -1,12 +1,10 @@
 import dev.jeka.core.api.depmanagement.*;
 import dev.jeka.core.api.java.JkJavaVersion;
 import dev.jeka.core.api.java.project.JkJavaProject;
-import dev.jeka.core.api.java.project.JkJavaProjectMaker;
-import dev.jeka.core.api.system.JkException;
-import dev.jeka.core.api.system.JkPrompt;
 import dev.jeka.core.api.tooling.JkGitWrapper;
 import dev.jeka.core.tool.JkCommands;
 import dev.jeka.core.tool.JkConstants;
+import dev.jeka.core.tool.JkEnv;
 import dev.jeka.core.tool.JkInit;
 import dev.jeka.core.tool.builtins.java.JkPluginJava;
 
@@ -21,23 +19,33 @@ class Build extends JkCommands {
 
     final JkPluginJava javaPlugin = getPlugin(JkPluginJava.class);
 
-    public String ossrhUsername;
+    @JkEnv("OSSRH_USER")
+    public String ossrhUser;
 
+    @JkEnv("OSSRH_PWD")
     public String ossrhPwd;
 
     @Override
     protected void setup() {
         JkJavaProject project = javaPlugin.getProject();
         JkGitWrapper git = JkGitWrapper.of(getBaseDir());
-        project.setVersionedModule("dev.jeka.plugins:springboot", git.getVersionWithTagOrSnapshot());
+
+        //Git drives project version numbering
+        String projectVersion = git.getVersionWithTagOrSnapshot();
+        project.setVersionedModule("dev.jeka.plugins:springboot", projectVersion);
         project.getCompileSpec().setSourceAndTargetVersion(JkJavaVersion.V8);
+
+        // Use same jeka version as the one for building
         project.addDependencies(JkDependencySet.of()
                 .andFile(getBaseDir().resolve(JkConstants.JEKA_DIR).resolve("boot/dev.jeka.jeka-core.jar"), PROVIDED));
-        project.setMavenPublicationInfo(mavenPublicationInfo());
-        if (!project.getVersionedModule().getVersion().isSnapshot()) {
+        if (!JkVersion.of(projectVersion).isSnapshot()) {
             javaPlugin.pack.javadoc = true;
-            javaPlugin.pack.sources = true;
         }
+
+        // Publication on Maven Central
+        javaPlugin.getProject().getMaker().getTasksForPublishing()
+                .setMavenPublicationInfo(mavenPublicationInfo())
+                .setPublishRepos(JkRepoSet.ofOssrhSnapshotAndRelease(ossrhUser, ossrhPwd));
     }
 
     protected JkMavenPublicationInfo mavenPublicationInfo() {
@@ -47,25 +55,6 @@ class Build extends JkCommands {
                 .withScm("https://github.com/jerkar/spring-boot-addin.git")
                 .andApache2License()
                 .andGitHubDeveloper("djeang", "djeangdev@yahoo.fr");
-    }
-
-    @Override
-    protected void setupAfterPluginActivations() {
-        JkJavaProjectMaker maker = javaPlugin.getProject().getMaker();
-        maker.setDependencyResolver(maker.getDependencyResolver().andRepos(
-                JkRepo.ofMavenOssrhDownloadAndDeploySnapshot(ossrhUsername, ossrhPwd).toSet()));
-        maker.getTasksForPublishing().setPublishRepos(JkRepoSet.ofOssrhSnapshotAndRelease(ossrhUsername, ossrhPwd));
-    }
-
-    public void release() {
-        JkGitWrapper git = JkGitWrapper.of(getBaseDir());
-        System.out.println("Existing tags :");
-        git.exec("tag");
-        String newTag = JkPrompt.ask("Enter new tag : ");
-        if (git.isDirty()) {
-            throw new JkException("Git workspace is dirty. Cannot put tag.");
-        }
-        git.tagAndPush(newTag);
     }
 
     public static void main(String[] args) {
--- dev.jeka.plugins.spring-boot/jeka/def/RunPublish.java
+++
@@ -1,9 +0,0 @@
-import dev.jeka.core.tool.JkInit;
-
-class RunPublish {
-
-    public static void main(String[] args) {
-        JkInit.instanceOf(Build.class, args).javaPlugin.publish();
-    }
-
-}
