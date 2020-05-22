eabf403680b51aec7974f03a34e6f8f9b7512c47
djeang <djeangdev@yahoo.fr>
2020-05-22 13:54:48 +0200

    Migrate to Jeka 0.9.0.M9

--- dev.jeka.plugins.spring-boot-sample/jeka/def/ApplicationBuildSample.java
+++ dev.jeka.plugins.spring-boot-sample/jeka/def/ApplicationBuildSample.java
@@ -1,5 +1,6 @@
 import dev.jeka.core.api.depmanagement.JkDependencySet;
 import dev.jeka.core.api.depmanagement.JkScope;
+import dev.jeka.core.api.java.testing.JkTestSelection;
 import dev.jeka.core.tool.JkCommandSet;
 import dev.jeka.core.tool.JkDefClasspath;
 import dev.jeka.core.tool.JkDoc;
@@ -9,7 +10,6 @@ import dev.jeka.plugins.springboot.JkPluginSpringboot;
 
 import java.nio.file.Path;
 
-
 @JkDefClasspath("dev.jeka:springboot-plugin:2.4.0.RC6")  // Add Springboot plugin to build classpath
 @JkDefClasspath("com.jcraft:jsch:0.1.55")  // SSH library to deploy on remote hosts
 class ApplicationBuildSample extends JkCommandSet {
@@ -18,20 +18,25 @@ class ApplicationBuildSample extends JkCommandSet {
 
     private final JkPluginSpringboot springboot = getPlugin(JkPluginSpringboot.class);
 
+    public boolean runIT = true;
+
     @JkDoc("The host to deploy Springboot application on.")
     public String host = "your.host";
 
     @Override
     protected void setup() {
         springboot.setSpringbootVersion("2.2.6.RELEASE");
-        java.getProject()
-            .getProduction()
-                .getDependencyManagement()
-                    .addDependencies(JkDependencySet.of()
-                        .and("org.springframework.boot:spring-boot-starter-web")
-                        .and("com.google.guava:guava:23.0")
-                        .and("org.springframework.boot:spring-boot-starter-test", JkScope.TEST)
-                            .withLocalExclusions("org.junit.vintage:junit-vintage-engine"));
+        java.getProject().getProduction()
+            .getDependencyManagement()
+                .addDependencies(JkDependencySet.of()
+                    .and("org.springframework.boot:spring-boot-starter-web")
+                    .and("com.google.guava:guava:23.0")
+                    .and("org.springframework.boot:spring-boot-starter-test", JkScope.TEST)
+                        .withLocalExclusions("org.junit.vintage:junit-vintage-engine")).__
+            .getTesting()
+                .getTestSelection()
+                    .addIncludeStandardPatterns()
+                    .addIncludePatternsIf(runIT, JkTestSelection.IT_INCLUDE_PATTERN);
     }
 
     public void cleanPack() {
@@ -49,14 +54,13 @@ class ApplicationBuildSample extends JkCommandSet {
         JkInit.instanceOf(ApplicationBuildSample.class, args).cleanPack();
     }
 
-
-
-
     private void copyToRemoteSsh(Path jar, String remotePath) {
     }
 
     private void restartSpringbootOnRemoteSsh() {
     }
 
-
 }
+
+
+
