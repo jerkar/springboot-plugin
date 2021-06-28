02fb0e718c49166d861af695e769bddfb23394bc
djeang <djeangdev@yahoo.fr>
2021-06-28 03:55:06 +0200

    template

--- dev.jeka.plugins.spring-boot/src/main/resources/snippet/Build.java
+++ dev.jeka.plugins.spring-boot/src/main/resources/snippet/Build.java
@@ -11,7 +11,7 @@ class Build extends JkClass {
 
     @Override
     protected void setup() {
-        springboot.setSpringbootVersion("2.3.1.RELEASE");
+        springboot.setSpringbootVersion("2.5.2");
         springboot.javaPlugin().getProject().simpleFacade()
             .setCompileDependencies(deps -> deps
                 .and("org.springframework.boot:spring-boot-starter-web")
